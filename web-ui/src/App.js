c952a0d236846f3f5fdc911dbe26a4e00a37f9f2
PlayerG9 <g9yout@gmail.com>
2022-03-06 17:23:46 +0100

    added development page

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import './generell.css'
 
 
 export default class App extends React.Component {
     render(){
-        return <>
-            <h1>Hello World</h1>
-        </>
+        return <div className='app'>
+            <h1>In Development</h1>
+            <div className='spinning'>
+                <p className='glow'>♫</p>
+            </div>
+        </div>
     }
 }
